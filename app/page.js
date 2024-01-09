8d80a1e58f09eab922dd1b0ad30168650b6b8fde
Ellie <camera20120109@gmail.com>
2024-01-10 00:22:33 +0800

    add chat input clear function

--- app/page.js
+++ app/page.js
@@ -1,7 +1,9 @@
 "use client"
 import Image from 'next/image';
-import React, {useState} from 'react';
+import React, {useState,useEffect} from 'react';
 import { auth, db } from '../firebaseConfig';
+import { space } from 'postcss/lib/list';
+import { collection, addDoc, updateDoc, doc, deleteDoc, } from "firebase/firestore";
 
 
 export default function Home() {
@@ -12,14 +14,16 @@ export default function Home() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
-  const [message, setMessage] = useState('');
-  const [send, setSend] = useState('')
+  const [message, setMessage] = useState([]);
+  const [send, setSend] = useState([])
 
   const inputMessage = (event) => { 
     setMessage(event.target.value) 
     } 
   const sendButton = () => {
-    setSend(message)
+    setSend([...send,message])
+    setMessage([])
+    // send: ['hello','h2'] => 'hello','h2' => ['hello','h2',message]    
   }
 
    const handleLogin = () => {
@@ -33,6 +37,21 @@ export default function Home() {
         // 处理错误
       });
   };
+
+const saveIntoFirestore = async(content) =>  {  
+
+    try {
+      console.log(123456)
+    await addDoc(collection(db, "chat-content"), {
+      gender:'male',
+      mbti:'entp',
+      content:content
+    });
+    console.log(123)
+    } catch (err) {
+      console.log('err:::',err)
+    }
+}
 
   return (
      <div>
@@ -66,6 +85,15 @@ export default function Home() {
         textAlign:'center' }}>
 
         <h1>16 Personality Debate World</h1>
+
+        <div style={{
+          display:'flex',  
+          alignItems:'center',
+          
+          }}>
+
+            <div style={{flexDirection:'column',
+                         }}>
         <button onClick={openChat} 
         style={{
           width:'90px',
@@ -76,26 +104,39 @@ export default function Home() {
           cursor: 'pointer',  
           alignItems:'center'        
         }} >
-          <div style={{
-                        width:'90px',
-                        height:'90px',
-                        borderRadius:'50%',
-                        overflow: 'hidden',
-                        position: 'absolute', // 设置容器为绝对定位
-                        left: '50%', // 向右移动 50% 的宽度
-                        
-                        transform: 'translate(-50%, -50%)' // 向左和向上各偏移自身 50% 的宽度和高度
-                      }}>
+          
           <Image
           src="/ENFP-DALL.png" 
           alt="Ellie"
-          width={110}
-          height={110}
+          width={120}
+          height={120}
           style={{ objectFit: 'cover',
-          objectPosition: 'left' }} 
+          objectPosition: 'left',
+          transform: 'translate(-10%, -5%)' }} 
           ></Image>
-          </div>
+          
         </button>
+        <p>ENFP</p>
+        </div>
+
+        <Image    src="/male.svg" 
+                  alt='male'
+                  width={70}
+                  height={70}
+                  style={{margin:'40px',top:'5px'}}></Image>
+        <Image    src="/female.svg" 
+                  alt='female'
+                  width={70}
+                  height={70}
+                  style={{}}></Image>
+
+       
+
+
+        </div>
+
+        
+        
         
       </div>
       
@@ -117,18 +158,16 @@ export default function Home() {
                   overflowY: 'scroll', 
                 }}> 
 
-                <div style={{ flex: 1, overflowY: 'auto' }}>
-                       {/* {messages.map((message, index) => (
-                      <div key={index} style={{ margin: '10px 0' }}>
-                        {message}
-                       </div>
-                          ))} */}
-                          {send}
-
+                <div style={{ flex:'reverse', overflowY: 'auto', margin:'2px'}}>
+                         {send.map((mes,i)=>{
+                            return (<div className={'bg-green-500 m-2'} key={i}>{mes}</div>)
+                         })}
+                   {/* {send} */}
+                            
                 </div>
                 
                 <div style={{ position: 'relative', width:'100%',height:'30px' }}>
-                  <input onChange={inputMessage} style={{
+                  <input value={message} onChange={inputMessage} style={{
                   position: 'absolute',                  
                   button:'20px',
                   width:'calc(100% - 30px)',
@@ -141,8 +180,7 @@ export default function Home() {
                   src="/send-alt-svgrepo-com.svg" 
                   alt='send'
                   width={0}
-                  height={0}
-                  type="submit"
+                  height={0}                  
                   style={{
                     position: 'absolute',
                     right: '1px',
